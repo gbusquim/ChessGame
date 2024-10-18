5864b34ba8c9ed0c450e012eade6877fe42d0d7f
Gabriel <gbtatu@hotmail.com>
2024-10-17 23:49:58 -0300

    add online game options in the menu

--- src/components/menu/menu.component.ts
+++ src/components/menu/menu.component.ts
@@ -1,15 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule, FormsModule],
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent {
   router: Router;
+  showInputJoinGame: boolean = false;
+  gameId!: string;
 
   constructor(router: Router) {
     this.router = router;
@@ -19,5 +23,16 @@ export class MenuComponent {
     this.router.navigate(['/mainpage']);
   }
 
-  goToOnlineGame() {}
+  createOnlineGame() {
+    const randomGameId = Math.floor(10000 + Math.random() * 90000);
+    this.router.navigate(['online','playerOne', randomGameId]);
+  }
+
+  showGameCodeInput() {
+    this.showInputJoinGame = true;  
+  }
+
+  joinOnlineGame() {
+    this.router.navigate(['online','playerTwo', this.gameId]);
+  }
 }
